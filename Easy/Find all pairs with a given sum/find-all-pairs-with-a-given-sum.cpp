2345bedfe2f6b5d77646dda0f7225825027debf3
SAKSHI TRIPATHI <123804307+sakshitripathi28@users.noreply.github.com>
2023-05-19 23:22:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find all pairs with a given sum/find-all-pairs-with-a-given-sum.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        // Your code goes here  
+        
+        set<int> s;
+        vector<pair<int,int>> v;
+        for(int x=0;x<M;x++)
+        {
+            s.insert(B[x]);
+        }
+        
+        sort(A,A+N);
+        for(int i=0;i<N;i++)
+        {
+            int x = X - A[i];
+            if(s.find(x) != s.end())
+            {
+                v.push_back({A[i],x});
+                s.erase(x);
+            }
+                
+        }
+        
+        return v;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}
+// } Driver Code Ends
