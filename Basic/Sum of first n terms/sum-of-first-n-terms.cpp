e788b3826f5de2bc9b8b7663f715455131005a4d
SAKSHI TRIPATHI <123804307+sakshitripathi28@users.noreply.github.com>
2023-05-23 10:36:55 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Sum of first n terms/sum-of-first-n-terms.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long sumOfSeries(long long N) {
+        // code here
+        if(N==0)
+            return 0;
+
+        long long cube = pow(N,3);
+        long long ans = sumOfSeries(N-1) + cube;
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.sumOfSeries(N) << "\n";
+    }
+}
+// } Driver Code Ends
