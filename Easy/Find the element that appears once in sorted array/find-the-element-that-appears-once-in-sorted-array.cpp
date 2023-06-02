6cf10ecca7ee0a44f62df888bbed7f09c63597ac
SAKSHI TRIPATHI <123804307+sakshitripathi28@users.noreply.github.com>
2023-06-03 00:16:41 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the element that appears once in sorted array/find-the-element-that-appears-once-in-sorted-array.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Driver code
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int findOnce(int arr[], int n)
+    {
+        //code here.
+        int low = 0,high = n-1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            
+            if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1])
+                return arr[mid];
+            else if(mid%2==0)
+            {
+                if(arr[mid]==arr[mid+1])
+                low=mid+1;
+                else
+                high=mid-1;
+            }
+            else
+            {
+                if(arr[mid]==arr[mid-1])
+                low=mid+1;
+                else
+                high=mid-1;
+            }
+                
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+        {
+            cin>>A[i];
+        }
+        
+        Solution ob;
+        
+        int res = ob.findOnce(A,n);
+        cout << res << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
