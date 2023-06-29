8ffe1792d3b9ec884b5b29fc9bd7d3347719f79c
SAKSHI TRIPATHI <123804307+sakshitripathi28@users.noreply.github.com>
2023-06-29 09:23:52 +0530

    Updated Solution - GfG to GitHub

--- Easy/Palindrome String/palindrome-string.cpp
+++ Easy/Palindrome String/palindrome-string.cpp
@@ -8,27 +8,65 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
+// 	bool palindrome(string S,int i)
+// 	{
+// 	    if(i >= S.length()/2)
+// 	        return true;
+	        
+// 	   if(S[i] != S[S.length()-i-1])
+// 	   {
+// 	       return false;
+// 	   }
+	   
+// 	   return palindrome(S,i+1);
+// 	}
 	
-	
-	int isPalindrome(string S)
+	int isPalindrome(string s)
 	{
 	    
 	    // Your code goes here
+	   int i=0;
+       int j=s.size()-1;
+       
+       while(i<j)
+       {
+          
+          if(s[i]!=s[j])
+          {
+              return 0;
+          }
+          
+          i++;
+          j--;
 	    
+       }
+	   // if(S.length() == 1)
+	   // {
+	   //     return 1;
+	   // }
 	    
-	   if(S.length() <= 1)
-	        return 1;
+	   // int i=0;
+	    
+	   // if(palindrome(S,i))
+	   // {
+	   //     return 1;
+	   // }
+	   // else
+	   //     return 0;
+	    
+	   //if(S.length() <= 1)
+	   //     return 1;
 	        
-	   if(S[0] == S[S.length()-1])
-	   {
+	   //if(S[0] == S[S.length()-1])
+	   //{
 	   
-	       return isPalindrome(S.substr(1,S.length()-2));
-	   }
-	   else
-	    return 0;
+	   //    return isPalindrome(S.substr(1,S.length()-2));
+	   //}
+	   //else
+	   // return 0;
 	   
 	   
-	    
+	  return 1;  
 	}
 
 };
