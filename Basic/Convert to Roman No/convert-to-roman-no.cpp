8cae6ebe4dcc0f2ffcf171e6ee2a8feb7d5f8395
SAKSHI TRIPATHI <123804307+sakshitripathi28@users.noreply.github.com>
2023-07-18 00:33:46 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Convert to Roman No/convert-to-roman-no.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    string convertToRoman(int n) {
+        // code here
+        string symbol[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int val[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string ans = "";
+        
+        for(int i=0;i<13;i++)
+        {
+            while(n >= val[i])
+            {
+                ans += symbol[i];
+                n = n - val[i];
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int N;
+		cin>>N;
+		Solution ob;
+    	cout << ob.convertToRoman(N) << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
