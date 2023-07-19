8ad21420457b6388a52bdea86568a02542172c98
SAKSHI TRIPATHI <123804307+sakshitripathi28@users.noreply.github.com>
2023-07-19 23:52:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Form a palindrome/form-a-palindrome.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int countMin(string s){
+    //complete the function here
+    int n = s.size();
+        string a = s;
+        reverse(a.begin(),a.end());
+
+        vector<vector<int>> dp(n+1 , vector<int>(n+1,0));
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                if(s[i-1] == a[j-1])
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return n-dp[n][n];
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
