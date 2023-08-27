355b3558cfbe0bbeed1c44027274c817a033d272
SAKSHI TRIPATHI <123804307+sakshitripathi28@users.noreply.github.com>
2023-08-27 06:57:03 +0530

    Updated Solution - GfG to GitHub

--- Easy/Selection Sort/selection-sort.cpp
+++ Easy/Selection Sort/selection-sort.cpp
@@ -19,30 +19,24 @@ class Solution
         int mini = i;
         for(int j=i+1;j<n;j++)
         {
-            
             if(arr[j] < arr[mini])
             {
                 mini = j;
             }
-            
-            // cout<< mini;
         }
         
         return mini;
-        
     }
      
     void selectionSort(int arr[], int n)
     {
        //code here
-       for(int i=0;i<n-1;i++)
+       for(int i =0;i<n-1;i++)
        {
            int mini = select(arr,i,n);
-           
-           swap(arr[i],arr[mini]);
-           
+           swap(arr[i] , arr[mini]);
+        
        }
-       
     }
 };
 
